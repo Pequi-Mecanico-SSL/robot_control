1df54fc80230a2f0978089b7ea52b6a0f82f7b20
Thiago Henrique <thiago.017henrique@gmail.com>
2025-06-21 10:42:40 -0300

    Use i2c communication

--- robot_control/stm32_bridge.py
+++ robot_control/stm32_bridge.py
@@ -5,21 +5,7 @@ from std_msgs.msg import Float32MultiArray
 import spidev
 import struct
 from threading import Lock
-import smbus2
-
-def to_byte_list_spi(data):
-    tx_bytes = struct.pack('ffff', *data)
-    return list(tx_bytes)
-
-def from_byte_list_spi(data):
-    rx_bytes = bytes(data)
-    return list(struct.unpack('ffff', rx_bytes))
-
-def to_byte_list_i2c(data):
-    return list(struct.pack('<4f', *data))
-
-def from_byte_list_i2c(data):
-    return list(struct.unpack('<4f', bytes(data)))
+from smbus2 import SMBus, i2c_msg
 
 class Stm32Bridge(Node):
     def __init__(self):
@@ -35,36 +21,42 @@ class Stm32Bridge(Node):
             'encoder_values',
             10)
         
-        self.bus  = smbus2.SMBus(1)
+        self.bus  = SMBus(0)
         self.addr = 0x42
         self.i2c_lock = Lock()
 
         self.last_encoder_published_time = self.get_clock().now()
         self.encoder_publish_timer = self.create_timer(0.1, self.publish_encoder_values_callback)
 
-    def publish_encoder_values(self, byte_list):
+    def publish_encoder_values(self, encoder_values):
         msg = Float32MultiArray()
-        encoder_values = from_byte_list_i2c(byte_list)
         msg.data = encoder_values
         self.last_encoder_published_time = self.get_clock().now()
         self.publisher.publish(msg)
+    
+    def write_and_read(self, floats_out):
+        with self.i2c_lock:
+            # Write 16 bytes to the STM32
+            tx_bytes = struct.pack('<4f', *floats_out)
+            self.bus.i2c_rdwr(i2c_msg.write(self.addr, tx_bytes))
+            # Read 16 bytes back from the STM32
+            rx = i2c_msg.read(self.addr, 16)
+            self.bus.i2c_rdwr(rx)
+            floats_in = struct.unpack('<4f', bytes(rx))
+            return list(floats_in)
 
     def motor_commands_callback(self, msg):
         cmd_list = [float(x) for x in msg.data][:4] # Limit to 4 commands
-        tx_byte_list = to_byte_list_i2c(cmd_list)
-        with self.i2c_lock:
-            rx_byte_list = self.bus.write_i2c_block_data(self.addr, 0x00, tx_byte_list)
-        self.publish_encoder_values(rx_byte_list)
+        encoder_values = self.write_and_read(cmd_list)
+        self.publish_encoder_values(encoder_values)
 
     def publish_encoder_values_callback(self):
         now = self.get_clock().now()
         time_since_last_publish = (now - self.last_encoder_published_time).nanoseconds / 1e9
         if time_since_last_publish > 0.1:
             null_cmd_list = [-1.0, -1.0, -1.0, -1.0]  # Dummy command to trigger encoder read
-            null_cmd_byte_list = to_byte_list(null_cmd_list)
-            with self.i2c_lock:
-                rx_byte_list = self.bus.read_i2c_block_data(null_cmd_byte_list)
-            self.publish_encoder_values(rx_byte_list)
+            encoder_values = self.write_and_read(null_cmd_list)
+            self.publish_encoder_values(encoder_values)
 
 def main(args=None):
     rclpy.init(args=args)
